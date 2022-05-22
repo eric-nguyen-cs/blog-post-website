59cd75b7edefbaabea96b3c61eaca7edba1d76a6
Eric Nguyen <ericnguyen.developper@gmail.com>
2022-05-22 18:50:16 +0200

    Add /login page

---
+++ src/frontend/pages/login.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import Router from 'next/router'
+import Layout from '../components/Layout'
+import { useAuthContext } from '../services/auth'
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState('')
+
+  const { login } = useAuthContext()
+  const submitData = async (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    try {
+      const credentials = { email }
+      await login(credentials)
+      await Router.push("/")
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return (
+    <Layout>
+      <div className="page">
+        <form
+          onSubmit={submitData}>
+          <h1>Login</h1>
+          <input
+            onChange={e => setEmail(e.target.value)}
+            placeholder="Email address"
+            type="text"
+            value={email}
+          />
+          <input
+            disabled={!email}
+            type="submit"
+            value="Login"
+          />
+          <a className="back" href="#" onClick={() => Router.push("/")}>
+            or Cancel
+        </a>
+        </form>
+      </div>
+      <style jsx>{`
+      .page {
+        background: white;
+        padding: 3rem;
+        display: flex;
+        justify-content: center;
+      }
+
+      input[type='text'] {
+        width: 100%;
+        padding: 0.5rem;
+        margin: 0.5rem 0;
+        border-radius: 0.25rem;
+        border: 0.125rem solid rgba(0, 0, 0, 0.2);
+      }
+
+      input[type='submit'] {
+        background: #ececec;
+        border: 0;
+        padding: 1rem 2rem;
+      }
+
+      .back {
+        margin-left: 1rem;
+      }
+    `}</style>
+    </Layout>
+  )
+}
+
+export default Login
